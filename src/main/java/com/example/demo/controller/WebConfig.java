2ba23c3739796f1edf2d0c3abc8129753b0b544e
Pia Hirsch <pia.hirsch.04gmail.com>
2025-05-25 12:28:02 +0200

    render deployment fixen

--- src/main/java/com/example/demo/controller/WebConfig.java
+++ src/main/java/com/example/demo/controller/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("http://localhost:5173");
+                .allowedOrigins("http://kitchenmate-frontend.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     };
 }
